ee54ca78166bbbb09a8255235deed473e4e535ff
xieshengyong <m@snowzen.cn>
2021-08-26 17:44:17 +0800

    update: 优化View基类的instace实例注入逻辑

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,16 +1,13 @@
 import '../less/style.less';
 
-import View from './app/tool/View';
 // 引入的包根据实际情况而定
 import LoadViewController from './app/LoadViewController';
 import IndexViewController from './app/IndexViewController';
 
 (function () {
-    const ViewInstance = new View();
-
     // load页面
     const loadPageBack = () => {
-        let loadView = new LoadViewController(ViewInstance);
+        let loadView = new LoadViewController();
 
         loadView.on('showIndex', indexPageBack);
 
@@ -19,7 +16,7 @@ import IndexViewController from './app/IndexViewController';
 
     // 长图页面
     const indexPageBack = (debug: boolean) => {
-        let indexView = new IndexViewController(ViewInstance);
+        let indexView = new IndexViewController();
 
         indexView.show(debug);
     };
