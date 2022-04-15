4753092bde6e9113de8063311f5cd5bce99ef8d7
xieshengyong <m@snowzen.cn>
2022-04-15 19:17:36 +0800

    update: 优化rem布局方案

--- src/js/app/util/rem.js
+++ src/js/app/util/rem.js
@@ -1,39 +1,48 @@
 (function (win, doc) {
     if (!win.addEventListener) return;
-    var rootValue = 100;  // 此处值与postcss配置中'postcss-pxtorem'的值一样
+    var ratio = 100;  // 此处值与postcss配置中'postcss-pxtorem'的值一样
     var html = document.documentElement;
+    let long, short;
 
     function setFont () {
-        var k = 750;
-        var scale = html.clientWidth / k;
-        window.scaleNum = scale;
-        var resultValue = (scale * rootValue).toFixed(2);
+        var resultValue;
 
+        console.log(html.clientWidth, html.clientHeight);
         if (html.clientWidth > html.clientHeight) {
-            if (html.clientWidth / html.clientHeight <= 1.926) {
-                resultValue = html.clientWidth / 1310;
-            } else {
-                resultValue = html.clientHeight / 680;
-            }
+            long = html.clientWidth;
+            short = html.clientHeight;
+        } else {
+            long = html.clientHeight;
+            short = html.clientWidth;
         }
 
-        if (resultValue > 80) {
-            html.style.fontSize = '80px';
+        if (long / short >= 2.16) {
+            resultValue = (long * ratio / 1624).toFixed(2);
+            console.log(1);
+        } else if (long / short <= 1.6) {
+            resultValue = (long * ratio / 1200).toFixed(2);
+            console.log(2);
+        } else {
+            resultValue = (short * ratio / 750).toFixed(2);
+            console.log(3);
+        }
+
+        if (resultValue > 114) {
+            html.style.fontSize = '114px';
             (html.className.indexOf('pad') <= -1) && (html.className += ' pad');
+            let wrap = document.querySelector('.m-horizontal .m-wrap');
+            if (wrap) {
+                wrap.style.height = '7.5rem'; wrap.style.width = '16.24rem';
+            }
         } else {
             html.style.fontSize = resultValue + 'px';
         }
-        // console.log(html.clientHeight / k);
-        document.querySelectorAll('.scale-dom').forEach((dom) => {
-            dom.style.webkitTransform = 'scale(' + scale + ')';
-            dom.style.transform = 'scale(' + scale + ')';
-        });
 
         // 老年模式，兼容微信右上字体调节, 不支持动态调节；
-        var htmlFontSize = parseFloat(window.getComputedStyle(html).fontSize).toFixed(2);
-        if (htmlFontSize !== resultValue) {
-            html.style.fontSize = (resultValue / (htmlFontSize / resultValue)).toFixed(2) + 'px';
-        }
+        // var htmlFontSize = parseFloat(window.getComputedStyle(html).fontSize).toFixed(2);
+        // if (htmlFontSize !== resultValue) {
+        //     html.style.fontSize = (resultValue / (htmlFontSize / resultValue)).toFixed(2) + 'px';
+        // }
     }
     setFont();
     setTimeout(function () {
