37d9c09a6c3d6299aa447803d8cf91afd6463152
xieshengyong <m@snowzen.cn>
2022-07-22 18:35:15 +0800

    update：优化rem计算

--- src/js/app/util/rem.js
+++ src/js/app/util/rem.js
@@ -3,11 +3,14 @@
     var ratio = 100;  // 此处值与postcss配置中'postcss-pxtorem'的值一样
     var html = document.documentElement;
     let long, short;
+    let desWidth = 1624;
+    let desWidthSafe = 1200;
+    let desHeight = 750;
 
     function setFont () {
         var resultValue;
+        let isPC = !navigator.userAgent.match(/AppleWebKit.*Mobile.*/);
 
-        console.log(html.clientWidth, html.clientHeight);
         if (html.clientWidth > html.clientHeight) {
             long = html.clientWidth;
             short = html.clientHeight;
@@ -16,26 +19,38 @@
             short = html.clientWidth;
         }
 
-        if (long / short >= 2.16) {
-            resultValue = (long * ratio / 1624).toFixed(2);
-            console.log(1);
-        } else if (long / short <= 1.6) {
-            resultValue = (long * ratio / 1200).toFixed(2);
-            console.log(2);
-        } else {
-            resultValue = (short * ratio / 750).toFixed(2);
-            console.log(3);
-        }
-
-        if (resultValue > 114) {
-            html.style.fontSize = '114px';
-            (html.className.indexOf('pad') <= -1) && (html.className += ' pad');
-            let wrap = document.querySelector('.m-horizontal .m-wrap');
-            if (wrap) {
-                wrap.style.height = '7.5rem'; wrap.style.width = '16.24rem';
+        if (isPC) {
+            if (long / short >= 1.77) {
+                resultValue = (long * ratio / 1920).toFixed(2);
+            } else {
+                resultValue = (short * ratio / 1080).toFixed(2);
             }
         } else {
+            if (long / short >= 2.16) {
+                resultValue = (long * ratio / desWidth).toFixed(2);
+            } else if (long / short <= 1.6) {
+                resultValue = (long * ratio / desWidthSafe).toFixed(2);
+            } else {
+                resultValue = (short * ratio / desHeight).toFixed(2);
+            }
+        }
+
+        if (isPC) {
+            (html.className.indexOf('pc') <= -1) && (html.className += ' pc');
             html.style.fontSize = resultValue + 'px';
+        } else {
+            html.className = html.className.replace(' pc', '');
+            if (resultValue > 114) {
+                html.style.fontSize = '114px';
+                (html.className.indexOf('pad') <= -1) && (html.className += ' pad');
+                let wrap = document.querySelector('.m-horizontal .m-wrap');
+                if (wrap) {
+                    wrap.style.height = '7.5rem';
+                    //  wrap.style.width = '16.24rem';
+                }
+            } else {
+                html.style.fontSize = resultValue + 'px';
+            }
         }
 
         // 老年模式，兼容微信右上字体调节, 不支持动态调节；
